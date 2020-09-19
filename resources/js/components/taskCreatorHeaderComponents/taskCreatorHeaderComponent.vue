bc90c1851c23e76de287d37cb7e3a2399ded618a
aymomeni <aymomeni@gmail.com>
2020-09-19 13:00:28 -0600

    priority, effort selection added

--- resources/js/components/taskCreatorHeaderComponents/taskCreatorHeaderComponent.vue
+++ resources/js/components/taskCreatorHeaderComponents/taskCreatorHeaderComponent.vue
@@ -1,11 +1,10 @@
 <template>
-    <b-card
-        title="add/edit your task"
-    >
+    <b-card>
         <b-form-group>
             <b-row>
                 <b-col>
                     <b-form-input
+                        id="task-title"
                         v-model="title"
                         placeholder="Task Title"
                         class="mb-1"
@@ -13,6 +12,7 @@
                     </b-form-input>
 
                     <b-form-textarea
+                        id="task-description"
                         v-model="description"
                         placeholder="Task Description"
                     >
@@ -21,10 +21,17 @@
                 <b-col>
                     <b-row>
                         <b-col>
-                                <label for="effort-rating-inline">Effort:</label>
-                                <b-form-rating id="effort-rating-inline" inline v-model="value" :locale="locale" color="#ff8800" show-value precision="1" variant="info" size="sm"></b-form-rating>
+                            <b-form-rating id="effort-rating-inline" color="rgb(64, 192, 128)" show-value v-model="priorityValue" size="sm" no-border></b-form-rating>
+                            <b-form-text class="ml-1">priority</b-form-text>
+                            
+                            <b-form-input id="range-2" v-model="effortValue" show-value type="range" min="1" max="12" step="1"></b-form-input>
+                            <b-form-text class="ml-1">
+                                effort&nbsp;&nbsp;<strong>{{ effortValue }}</strong>
+                                <b-form-text id="effortValue-label"></b-form-text>
+                            </b-form-text>
                         </b-col>
                         <b-col>
+
                         </b-col>
                     </b-row>
                 </b-col>
@@ -40,11 +47,24 @@ export default {
     data: () => {
         return {
             title: "",
-            description: ""
+            description: "",
+            priorityValue: 3,
+            effortValue: 6
         }
     }
 }
 </script>
 
 <style scoped>
+#task-title {
+    background-color: rgb(235, 255, 244);
+}
+
+#task-description {
+    background-color: rgb(235, 255, 244);
+}
+
+#effort-numeric-value-label {
+    font-size: 1.5rem;
+}
 </style>

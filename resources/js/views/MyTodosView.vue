e005256d306fa3676c1ab7bf67781bd8690faf58
aymomeni <aymomeni@gmail.com>
2020-09-21 21:14:07 -0600

    new taskListContainerComponents are functional

--- resources/js/views/MyTodosView.vue
+++ resources/js/views/MyTodosView.vue
@@ -5,17 +5,29 @@
             <taskCreatorHeaderComponent class="mt-2" />
             <div class="mt-4">
                 <div class="row">
-                    <div class="col-12 col-sm-12 col-lg-3 mt-2">
+                    <div class="col-12 col-sm-12 col-lg-3">
                         <DailyTasksContainerComponent />
                     </div>
-                    <div class="col-12 col-sm-12 col-lg-3 readme">               
-                        
+                    <div class="col-12 col-sm-12 col-lg-3">               
+                        <TaskListContainer 
+                            taskListContainerTitle="Goals"
+                            base_url="api/goals"
+                            type="goals"
+                        />
                     </div>
-                    <div class="col-12 col-sm-12 col-lg-3 readme">               
-                        
+                    <div class="col-12 col-sm-12 col-lg-3">               
+                        <TaskListContainer 
+                            taskListContainerTitle="Books or Resources"
+                            base_url="api/booksOrResources"
+                            type="booksOrResources"
+                        />
                     </div>
-                    <div class="col-12 col-sm-12 col-lg-3 readme">               
-                        
+                    <div class="col-12 col-sm-12 col-lg-3">               
+                        <TaskListContainer 
+                            taskListContainerTitle="Completed"
+                            base_url="api/completed"
+                            type="completed"
+                        />
                     </div>
                 </div>
             </div>
@@ -36,6 +48,11 @@
             DailyTasksContainerComponent,
             TaskCreatorHeaderComponent,
             TaskListContainer
+        },
+        data: () => {
+            return {
+                
+            }
         }
     }
 </script>
